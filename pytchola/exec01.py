c8e2a293bfaf0c5d30bbbf0648fdeccd7812854d
pedro5tanikawa <phlkawa@gmail.com>
2025-11-14 21:30:51 -0300

    sheesh

---
+++ pytchola/exec01.py
@@ -0,0 +1,25 @@
+'''
+Faça um programa para calcular a quantidade de latas de tintas para pintar
+uma parede. O programa deverá solicitar ao usuário, a altura (float) e o
+comprimento(float) da parede. Considere que a cobertura da tinta é de 1 litro
+para cada 3 metros quadrados e que a tinta é vendida em latas de 3,6 litros,
+que custam R$ 107,00. Informe ao usuário a quantidades de latas de tinta a
+serem compradas e o preço total.
+'''
+
+import math
+alt = float(input('qual a altura da parede a ser pintada?'))
+comp = float(input('agora, o comprimento da parede'))
+area = alt * comp 
+print(area)
+litros = area / 3 #litros gastos
+resultado1 = round(litros)
+print(resultado1)
+tinta = 3.6
+lata = round(litros) / tinta
+resultado2 = round(lata)
+print(resultado2)
+tintaVAL = round(lata) * 107
+print('numero latas:',resultado2)
+print('valor gasto:', tintaVAL)
+
---
+++ pytchola/exec2.py
@@ -0,0 +1,15 @@
+'''
+Um determinado prêmio de loteria saiu para um bolão de três amigos. Uma lei
+garante que todo prêmio de loteria deva pagar um imposto de 7% para os
+cofres estaduais. Do total descontado o imposto, os amigos irão dividir o
+prêmio da seguinte maneira:
+● O primeiro ganhador receberá 46%;
+● O segundo receberá 32%;
+● O terceiro receberá o restante; Faça um programa que leia o valor total
+do prêmio, calcule o desconto, o valor que cada um tem direito e
+imprima o total do prêmio, o prêmio descontado o imposto e a quantia
+recebida por cada um dos ganhadores.
+'''
+premiocru = float(input('qual o valor total do premio adquirido:'))
+leao = 0.07
+bros = 3
