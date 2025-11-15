90fb028af07d17ecd558fb4a5a906a82ef16bcf2
pedro5tanikawa <phlkawa@gmail.com>
2025-11-14 21:38:39 -0300

    kassiano

--- pytchola/exec2.py
+++ pytchola/exec2.py
@@ -12,4 +12,9 @@ recebida por cada um dos ganhadores.
 '''
 premiocru = float(input('qual o valor total do premio adquirido:'))
 leao = 0.07
-bros = 3
+premioLeao = premiocru * leao
+print('valor do premio ja com impostos deduzidos:',premioLeao)
+ganhador1 = 0.46
+ganhador2 = 0.32
+ganhador3 = premiocru - (ganhador1 + ganhador2)
+print(f)
